a217a7759c704b992abb4aa17a115954954b927f
Carl Winkler <carl@winkler.id.au>
2017-06-25 23:00:44 +0800

    Change default port to 3142

--- back/index.ts
+++ back/index.ts
@@ -10,17 +10,17 @@ const app = express()
 
 const router = express.Router()
 
-const staticPath = path.resolve(__dirname, '..', 'front')
-
 app.get('/taxonomy', async (_, res) => res.json(await getTaxonomy()))
 app.get('/content')
 
+const staticPath = path.resolve(__dirname, '..', 'front')
 app.use(express.static(staticPath))
+
 app.use(router)
 
 const server = http.createServer(app)
 
-const port = process.env.PORT || 3141
+const port = process.env.PORT || 3142
 server.listen(port, () => {
     console.log(`Listening on port ${port}`)
 })
